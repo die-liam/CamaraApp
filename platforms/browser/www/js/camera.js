227bc6367d04f5fc325179adfd5890709704a926
Diego Williams <diegoalanis@live.com.mx>
2017-02-19 20:27:08 -0600

    camara con filtros

---
+++ platforms/browser/www/js/camera.js
@@ -0,0 +1,84 @@
+// JavaScript Document
+var app={
+  inicio: function(){
+	this.iniciaFastClick();
+    this.iniciaBotones();
+  },
+  iniciaFastClick: function ()
+  {
+	  FastClick.attach(document.body);
+  },
+  iniciaBotones: function() {
+	  var buttonAction=document.querySelector('#button-action');
+	  buttonAction.addEventListener('click', function () {
+	  app.cargarFoto(Camera.PictureSourceType.CAMERA)
+	  });
+	  
+	  var filtersButtons = document.querySelectorAll('.button-filter');
+	  filtersButtons[0].addEventListener('click', function() { 
+	  app.aplicaFiltro('gray');
+	  });
+	  
+	  filtersButtons[1].addEventListener('click', function() { 
+	  app.aplicaFiltro('negative');
+	  });
+	  
+	  filtersButtons[2].addEventListener('click', function() { 
+	  app.aplicaFiltro('sepia');
+	  });
+	  
+	   var buttonGallery=document.querySelector('#button-gallery');
+	  buttonGallery.addEventListener('click', function(){
+	  app.cargarFoto(Camera.PictureSourceType.PHOTOLIBRARY);
+	  });
+	  
+  },
+  cargarFoto: function(pictureSourceType) {
+	var opciones = {
+		quality: 100,
+		sourceType: pictureSourceType,
+		destinationType: Camera.DestinationType.FILE_URI,
+		targetWidth: 300,
+		targetHeight: 300, 
+		alloeEdit: true, 
+		cameraDirection: 1,
+		correctOrientation:true,
+		saveToPhotoAlbum: true
+	};
+	navigator.camera.getPicture(app.fotoCargada, app.errorAlCargarFoto, opciones);
+  },
+  fotoCargada: function(imageURI){
+	  var img =document.createElement('img');
+	  img.onload=function() {
+	  	app.pintarFoto(img);
+	  }
+		  img.src=imageURI;
+  },
+  pintarFoto: function (img) {
+  var canvas = document.querySelector('#foto');
+  var context = canvas.getContext('2d');
+  canvas.width=img.width;
+  canvas.height=img.height;
+  context.drawImage(img, 0,0, img.width, img.height);
+  },
+  
+  errorAlCargarFoto: function (message) {
+  console.log('Fallo al tomar foto: '+message);
+  },
+  
+  aplicaFiltro: function (filterName) {
+	  var canvas = document.querySelector('#foto');
+	  var context=canvas.getContext('2d');
+	  imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+	  effects[filterName](imageData.data);
+	  context.putImageData(imageData,0,0);
+  
+  }
+};
+var imageData;
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function() {
+        app.inicio();
+    }, false);
+}
+
